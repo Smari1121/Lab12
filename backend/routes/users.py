6895cf5a38431ac24626b8436d682ef6c82b90e0
chana-chor-garam <kanishka.jepal@gmail.com>
2025-04-16 15:58:38 +0530

    Update users.py

--- backend/routes/users.py
+++ backend/routes/users.py
@@ -8,7 +8,7 @@ async def get_users_collection():
     from db import init_db
     return init_db()["users_collection"]
 
-@router.post("/")
+@router.get("/") #it should be get and not post, we can't have 2 posts buddy T^T
 async def get_users():
     collection = await get_users_collection()
     users = []
@@ -27,7 +27,7 @@ async def create_user(user: User):
 @router.delete("/{user_id}")
 async def delete_user(user_id: str):
     collection = await get_users_collection()
-    result = await collection.delete_all()
+    rresult = await collection.delete_one({"_id": ObjectId(user_id)}) #there is noting called delete_all
     if result.deleted_count:
         return {"status": "deleted"}
     raise HTTPException(status_code=404, detail="User not found")
