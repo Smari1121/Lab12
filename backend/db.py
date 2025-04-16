94d3be012e044006f757c719dcd5f57afeb51fc9
Arushi <iarushitandon@gmail.com>
2025-04-16 15:14:19 +0530

    msg

--- backend/db.py
+++ backend/db.py
@@ -2,7 +2,7 @@ from motor.motor_asyncio import AsyncIOMotorClient
 from dotenv import load_dotenv
 import os
 
-load_dotenv()  # 👈 Loads from .env in same folder
+load_dotenv()  # Loads from .env in same folder -itstandon
 
 def init_db():
     MONGO_URI = os.getenv("MONGO_URL", "mongodb://localhost:27017")
