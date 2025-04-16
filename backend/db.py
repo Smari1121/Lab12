eacdb3899931478b3c160829496f154f9c8c59cb
Arushi <iarushitandon@gmail.com>
2025-04-16 16:18:29 +0530

    msg

--- backend/db.py
+++ backend/db.py
@@ -9,7 +9,7 @@ def init_db():
     client = AsyncIOMotorClient(MONGO_URI)
     db = client["testdb"]
     return {
-        "items_collection": db["item"],
+        "items_collection": db["items"], #item changed to items-itstandon
         "users_collection": db["users"]
     }
     # Question for chocolate: How can we implement nosql syntax in mysql ???
